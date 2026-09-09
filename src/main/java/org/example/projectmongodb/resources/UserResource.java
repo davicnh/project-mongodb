44e27e516749509218b87eb256eeb4075faada66
Davi Cunha <davicunha910@gmail.com>
2026-09-09 09:37:45 -0300

    DTO class

--- src/main/java/org/example/projectmongodb/resources/UserResource.java
+++ src/main/java/org/example/projectmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package org.example.projectmongodb.resources;
 
 import org.example.projectmongodb.domain.User;
+import org.example.projectmongodb.dto.UserDTO;
 import org.example.projectmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
