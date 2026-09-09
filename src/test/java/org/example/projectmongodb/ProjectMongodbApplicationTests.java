64ad6231fdaebf14f9a3d3f1d06748d1e2515411
Davi Cunha <davicunha910@gmail.com>
2026-09-09 08:20:34 -0300

    First commit

---
+++ src/test/java/org/example/projectmongodb/ProjectMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
