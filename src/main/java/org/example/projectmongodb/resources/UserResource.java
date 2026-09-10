2c6b55033f16da0abb2b9a998d2a81023244895a
Davi Cunha <davicunha910@gmail.com>
2026-09-10 07:52:13 -0300

    Post Endpoint

--- src/main/java/org/example/projectmongodb/resources/UserResource.java
+++ src/main/java/org/example/projectmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package org.example.projectmongodb.resources;
 
+import org.example.projectmongodb.domain.Post;
 import org.example.projectmongodb.domain.User;
 import org.example.projectmongodb.dto.UserDTO;
 import org.example.projectmongodb.services.UserService;
@@ -51,5 +52,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
